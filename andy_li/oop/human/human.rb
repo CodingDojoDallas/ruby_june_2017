2089b537089bf683694690e44289ee8e59301199
iliandy <iandyli@gmail.com>
2017-06-06 17:41:13 -0500

    completed human assignment

---
+++ andy_li/oop/human/human.rb
@@ -0,0 +1,32 @@
+class Human
+  attr_accessor :name, :strength, :intelligence, :stealth, :health
+
+  def initialize(name)
+    @name = name
+    @strength, @intelligence, @stealth, @health = 3, 3, 3, 100
+  end
+
+  def attack(obj)
+    if obj.class.ancestors.include?(Human)
+      obj.health -= 10
+      p "#{self.name} attacked #{obj.name}"
+    end
+
+    return self
+  end
+
+  def display_health()
+    p @health
+    return self
+  end
+
+end
+
+bri = Human.new("bri")
+pats = Human.new("pats")
+
+bri.display_health
+pats.display_health
+
+bri.attack(pats)
+pats.display_health
