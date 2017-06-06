cb3b0b0a16ebe6ee09d55dcc963e7031cc17b45b
timber <mister.modistette@gmail.com>
2017-06-05 19:59:25 -0500

    methods exercise

---
+++ timber_modisette/ruby_fund/methods_exercies.rb
@@ -0,0 +1,41 @@
+# def hello_world
+#   puts "hello world"
+# end
+# hello_world
+
+# def say_hello(name1,name2)
+#   puts "hello, #{name1} and #{name2}"
+# end
+# say_hello("oscar","eduardo")
+
+# def say_hello(name1="oscar", name2="piff")
+#   "hello, #{name1} and #{name2}"
+# end
+# puts say_hello("oscar","eduardo")
+
+# def say_hello(name1,name2)
+#   if name1.empty? or name2.empty?
+#     return "who are you?"
+#   end
+#
+#   "hello, #{name1} and #{name2}"
+# end
+#
+# puts say_hello("piff","timber")
+
+# puts "ruby!" * 2
+# puts "chunky" << "bacon"
+# puts "chunky"[2..3]
+# puts "ruBy".capitalize
+# puts "".empty?
+# puts "team".include?("i")
+# puts "matz".length
+#
+# puts "hello" if 0
+# puts "hello" if ""
+# puts "goodbye" if []
+# puts "ruby" unless nil
+
+# puts "matz" unless false
+# puts "matz" unless nil
+# puts "mats" unless []
