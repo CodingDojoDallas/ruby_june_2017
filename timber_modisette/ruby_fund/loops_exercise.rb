0f60671c2303ff82811888b2a7d2a12234bc1a2b
timber <mister.modistette@gmail.com>
2017-06-05 19:58:57 -0500

    loops exercise

---
+++ timber_modisette/ruby_fund/loops_exercise.rb
@@ -0,0 +1,38 @@
+# $i = 0
+# $num = 5
+# begin
+#   puts "inside the loop i = #{$i}"
+#   $i += 1
+#   puts "$i is now 3 " unless $i == 3
+# end while $i < $num
+
+
+# for i in 0..5
+#   puts "value of local variable is #{i}"
+#   puts "i is now 3" if i == 3
+# end
+
+# i = 0
+# num = 5
+# while i <= num do
+#   puts "inside the loop i = #{i}"
+#   i += 1
+# end
+
+# i = 0
+# num = 5
+# while i < num do
+#   puts "insdie the loop i = #{i}"
+#   i += 1
+#   break if i == 2
+# end
+
+# for i in 0..5
+#   puts "value of local variable is #{i}"
+#   break if i == 2
+# end
+# 
+# for i in 0..5
+#   next if i == 2
+#   puts "value of local variable is #{i}"
+# end
