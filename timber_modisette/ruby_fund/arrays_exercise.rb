fa9cc73023fc84d3603604a1f49636511f4ba52a
timber <mister.modistette@gmail.com>
2017-06-05 19:54:31 -0500

    arrays exercise

---
+++ timber_modisette/ruby_fund/arrays_exercise.rb
@@ -0,0 +1,33 @@
+a = ["matt","piff","jake","eila","monic","dojo"]
+b = [5,6,9,3,1,2,4,7,8,10]
+c = ["dojo",9]
+d = %w{matz guido dojo choi john}
+
+# x = (a+b) - c
+# puts x
+# puts x.to_s
+
+
+
+# puts a[0]
+# puts a[1]
+#
+# puts b.class
+# puts b.shuffle.join("-")
+# puts a.to_s
+
+
+# a.delete("piff")
+# puts a
+
+# a.push("piff")
+# puts a
+
+# a.pop()
+# puts a
+
+# puts a.length
+
+# puts a.reverse!
+#
+# puts a
